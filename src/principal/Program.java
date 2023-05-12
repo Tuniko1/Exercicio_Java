ef6483c14e447b2405301a64c8eca8fa4a9e1e7b
Tuniko <arthur@gmail.com>
2023-05-12 18:01:50 -0300

    Commit de exercicio-java

---
+++ src/principal/Program.java
@@ -0,0 +1,21 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//Faça um programa que mostre a mensagem "Olá mundo" na tela.
+		Exercicio2 e2 = new Exercicio2();
+		e2.Method1();
+		e2.Method2();
+		e2.Method3();
+		e2.Method4();
+		e2.Method5();
+		e2.Method6();
+		e2.Method7();
+		}
+}	
+		
+
+
